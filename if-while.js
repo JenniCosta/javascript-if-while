0d1fbd57af35b222acf75455662ac3caa6aa78ee
Renan Rodrigo <renanrb@ime.usp.br>
2018-08-20 10:32:52 -0300

    Corrigir exemplos e adicionar comentarios

--- if-while.js
+++ if-while.js
@@ -1,26 +1,31 @@
 // Exemplo de IF
 
-// let numero = prompt("Digite aqui um número:");
+let numero = prompt("Digite aqui um número:");
 
-// if(isNaN(Number(numero)) || numero === null){
-//     console.log("Valor inválido");
-// }
-// else if(numero > 10){
-//     console.log("É um número maior que 10");
-// }
-// else if(numero < 10){
-//     console.log("É um número menor que 10");
-// }
-// else{
-//     console.log("É O NÚMERO 10!");
-// }
+if(isNaN(Number(numero)) || numero === null){
+    console.log("Valor inválido");
+}
+else if(numero > 10){
+    console.log("É um número maior que 10");
+}
+else if(numero < 10){
+    console.log("É um número menor que 10");
+}
+else{
+    console.log("É O NÚMERO 10!");
+}
 
 // Exemplo de WHILE
 
+// inicialização
 let limite = Number(prompt("Digite o limite do contador"));
-let contador = 1;
+let i = 1;
 
-while(contador <= limite){
-    console.log(contador);
-    contador = contador + 1;
+while(i <= limite){ // condição
+    console.log(i);
+
+    // atualização
+    // i = i + 1;
+    // i += 1;
+    i++;
 }
